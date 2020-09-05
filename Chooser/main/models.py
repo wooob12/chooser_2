1aa2f50953a7e6ad7235e1bae2b60630e278bf94
githubStrongIce <binghwa0610@gmail.com>
2020-09-06 01:56:58 +0900

    main - models.py-2020.09.06 1. debate, prefer 관련 모델 작성 2. 주석 달기

--- Chooser/main/models.py
+++ Chooser/main/models.py
@@ -13,46 +13,50 @@ class Member(models.Model):
     def __str__(self):
         return self.member_nick
 
-# class Prefer(models.Model):
-#     prefer_id = models.AutoField(primary_key=True) # PK
-#     prefer_topic = models.AutoField(max_length=50) # FK
-#     prefer_member_id = models.AutoField() # FK
-#     prefer_title = models.CharField(max_length=50)
-#     prefer_date = models.DateTimeField(auto_now=True)
-#     prefer_content = models.TextField()
-#     prefer_file = 
 
-# class Topic(models.Model):
-#     topic_id = models.AutoField(primary_key=True)# PK
-#     topic_content = 
-#     topic_start_date = 
-#     topic_end_date = 
+# 취향 페이지
+class Prefer(models.Model):
+    prefer_id = models.AutoField(primary_key=True) # PK
+    prefer_topic = models.ForeignKey(topic_id, on_delete=models.CASCADE) # FK
+    prefer_member_id = models.ForeignKey(member_id, on_delete=models.CASCADE) # FK
+    prefer_title = models.CharField(max_length=50)
+    prefer_date = models.DateTimeField(auto_now_add=True) # 수정해도 날짜가 안바뀜
+    prefer_content = models.TextField()
+    prefer_file = # models.FileField()
 
-# class Comment_prefer(models.Model):
-#     com_pre_id = models.AutoField(primary_key=True) # PK
-#     com_pre_member_id = # FK(member_id)
-#     com_pre_prefer_id = # FK(prefer_id)
-#     com_pre_content = 
+class Topic(models.Model):
+    topic_id = models.AutoField(primary_key=True)# PK
+    topic_content = models.TextField()
+    topic_start_date = # 예약설정이 가능할까?
+    topic_end_date = # 예약설정이 가능할까?
 
-# class Debate(models.Model):
-#     debate_id = models.AutoField(primary_key=Auto) # PK
-#     debate_pre_date =
-#     debate_img1 =
-#     debate_img2 =
-#     debate_img1_name1 =
-#     debate_img2_name2 =
+class Comment_prefer(models.Model):
+    com_pre_id = models.AutoField(primary_key=True) # PK
+    com_pre_member_id = models.ForeignKey(member_id, on_delete=models.CASCADE) # FK(member_id)
+    com_pre_prefer_id = models.ForeignKey(prefer_id, on_delete=models.CASCADE) # FK(prefer_id)
+    com_pre_content = models.TextField()
 
-# class Comment_dabate(models.Model):
-#     com_deb_id = models.AutoField(primary_key=Auto) # PK
-#     com_deb_member_id = # FK(member_id)
-#     com_deb_content = # FK(debate_id)
-#     con_deb_date = 
 
-# class Vote(models.model):
-#     vote_id = models.AutoField(primary_key=Auto) # PK
-#     vote_member_id = # FK(member_id)
-#     vote_debate_id = # FK(debate_id)
-#     vote_result = 
+# 토론 페이지
+class Debate(models.Model):
+    debate_id = models.AutoField(primary_key=Auto) # PK
+    debate_pre_date = models.DateTimeField(auto_now_add=True) # 수정해도 날짜가 안바뀜
+    debate_img1 = models.imageField
+    debate_img2 = models.imageField
+    debate_img1_name1 = models.CharField(max_length=20)
+    debate_img2_name2 = models.CharField(max_length=20)
+
+class Comment_debate(models.Model):
+    com_deb_id = models.AutoField(primary_key=Auto) # PK
+    com_deb_member_id = models.ForeignKey(member_id, on_delete=models.CASCADE) # FK(member_id)
+    com_deb_content = models.ForeignKey(debate_id, on_delete=models.CASCADE) # FK(debate_id)
+    con_deb_date = models.DateTimeField(auto_now_add=True) # 수정해도 날짜가 안바뀜
+
+class Vote(models.model):
+    vote_id = models.AutoField(primary_key=Auto) # PK
+    vote_member_id = models.ForeignKey(member_id, on_delete=models.CASCADE)# FK(member_id)
+    vote_debate_id = models.ForeignKey(debate_id, on_delete=models.CASCADE)# FK(debate_id)
+    vote_result = # BooleanField(참거짓)으로 구분할 수 있을까? 언제든지 취소 수정 가능?
 
 
 
