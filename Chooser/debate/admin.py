5fad9576781de90f7ca264e879f6668049021967
githubStrongIce <binghwa0610@gmail.com>
2020-09-06 02:03:10 +0900

    debate - admin.py - 2020.09.06 1. admin.py: Debate 모델을 들고와 적용

--- Chooser/debate/admin.py
+++ Chooser/debate/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+# 다른 파일에서 모델 들고오기
+from .models import Debate, Comment_debate, Vote
 
-# Register your models here.
+# admin page에 model 등록
+admin.site.register(Debate)
+admin.site.register(Comment_debate)
+admin.site.register(Vote)
